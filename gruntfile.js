981773c29477e4b72779168a340ef2e5829e1c38
John <john.aspinall73@gmail.com>
2017-02-23 17:05:50 +0000

    First commit

---
+++ gruntfile.js
@@ -0,0 +1,106 @@
+module.exports = function(grunt) {
+
+    // 1. All configuration goes here 
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+
+        concat: {   
+            dist: {
+                src: [
+                    'js/libs/*.js', // All JS in the libs folder
+                    'js/global.js'  // This specific file
+                ],
+                dest: 'js/build/production.js',
+            }
+        },
+
+        uglify: {
+            options: {
+              mangle: false
+            },
+            my_target: {
+              files: {
+                'js/build/production.min.js': ['js/build/production.js']
+              }
+            }
+          },
+
+        imagemin: {
+            dynamic: {
+                files: [{
+                    expand: true,
+                    cwd: 'images/',
+                    src: ['**/*.{png,jpg,gif}'],
+                    dest: 'images/build/'
+                }]
+            }
+        },
+
+        sass: {
+            dist: {
+              files: [{
+                expand: true,
+                cwd: 'css',
+                src: ['*.scss'],
+                dest: 'css/build/',
+                ext: '.css'
+              }]
+            }
+          },
+
+        serve: {
+            options: {
+                port: 9000
+            }
+        },
+
+        watch: {
+            options: {
+                livereload: true,
+            },              
+            css: {
+                files: ['css/**/*.scss'],
+                tasks: ['sass'],
+                options: {
+                    spawn: false,
+                    livereload: true,
+                }
+            },            
+            scripts: {
+                files: ['js/*.js'],
+                tasks: ['concat', 'uglify'],
+                options: {
+                    spawn: false,
+                    livereload: true,                    
+                },
+            },
+            html: {
+                files : ['./*.htm'],
+                options: {
+                    livereload: true,
+                }
+            } 
+        },
+
+        concurrent: {
+            target1: ['serve', 'watch', 'imagemin'],
+        }
+
+
+
+    });
+
+    // Load all Grunt tasks automatically wihtout having to enter manaually
+    require('load-grunt-tasks')(grunt);
+
+    grunt.registerTask(
+        'default',
+            [
+                'concat', 
+                'uglify', 
+                'sass', 
+                'concurrent:target1'
+            ]
+    );
+
+};
